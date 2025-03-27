332305edc3d77dcfc3eb55ab330ee70bd7fb354c
ELSAAA\elsab <umutoni.bucagu@strathmore.edu>
2025-03-27 15:07:02 +0300

    Added the Jarfreecharts libraries and made changes to the student and course pages. Now one can insert, update or delete a student in the student tabel or a course in the course table.

--- src/DatabaseManager/Db_connect.java
+++ src/DatabaseManager/Db_connect.java
@@ -7,9 +7,9 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
 public class Db_connect {
-    private static final String URL = "jdbc:postgresql://localhost:5432/learnspace";
+    private static final String URL = "jdbc:postgresql://localhost:5432/LearnspaceDB";
     private static final String USER = "postgres";
-    private static final String PASSWORD = "passcode";
+    private static final String PASSWORD = "Mignonne2004!@";
 
     private Db_connect() {
     }
